48f784b0dd66159e70074729c5ac89152fb2609a
baothi246@gmail.com <baothi246@gmail.com>
2015-01-07 17:59:35 +0700

    login

--- beginningrails4/config/routes.rb
+++ beginningrails4/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
+  resources :session, :only => [:new, :create, :destroy]
+  get '/login' => "sessions#new", :as => "login"
+
+  get '/logout' => "Sessions#destroy", :as => "logout"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
